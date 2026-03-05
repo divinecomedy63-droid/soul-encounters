bff476886f2cfea33100e6ee0e748d1a875e8890
divinecomedy63-droid <divinecomedy63@gmail.com>
2026-03-05 15:59:12 -0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'soul-encounters-v1';
+const CACHE_NAME = 'soul-encounters-v2';
 const urlsToCache = [
-  '/soul-encounters/',
-  '/soul-encounters/index.html'
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install event - cache essential files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -12,28 +13,20 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch event - serve from cache when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
-// Activate event - clean up old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
